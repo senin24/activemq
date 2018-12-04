943e87c4159aa4a8ce84a17a9e2190a2a673cd7b
senin24 <senin24@gmail.com>
2018-12-04 11:39:32 +0300

    init

---
+++ src/main/java/com/github/senin24/jms/activemq/ActivemqApplication.java
@@ -0,0 +1,12 @@
+package com.github.senin24.jms.activemq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivemqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivemqApplication.class, args);
+	}
+}
